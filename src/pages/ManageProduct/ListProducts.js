9e8b19c99774b3dd48b66969877a538ba94dc3d8
FHaouae <hawaefarhat@gmail.com>
2023-10-27 09:56:35 +0200

    add product cart

---
+++ src/pages/ManageProduct/ListProducts.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+const ListProduct = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        api.get('/products')
+            .then(response => {
+                setProducts(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching products:', error);
+            });
+    }, []);
+
+    return (
+        <div className="d-flex flex-wrap">
+            {products.map((product) => (
+                <Card key={product._id} style={{ width: '18rem', margin: '1rem' }}>
+                    <Card.Img variant="top" src={`images/${product.image}`} alt={product.nom} />
+                    <Card.Body>
+                        <Card.Title>{product.nom}</Card.Title>
+                        <Card.Text>Sport: {product.sport} </Card.Text>
+                        <Card.Text>Qte: {product.quantite_disponible} </Card.Text>
+                        <Card.Text>Price: {product.prix_location} €</Card.Text>
+                        <Button variant="primary">
+                            <Link to={`/cart/${product._id}`} style={{ color: 'white', textDecoration: 'none' }}>Add To Cart</Link>
+                        </Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </div>
+    );
+};
+
+export default ListProduct;
